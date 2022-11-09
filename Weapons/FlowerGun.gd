d3aaa8155c4f3005ec001c9d11a2d6e3bea0c2de
Kevin Oh <aflashyrhetoric@gmail.com>
2022-11-09 02:23:39 -0500

    Keep the timescale warp on flower gun firing

--- Weapons/FlowerGun.gd
+++ Weapons/FlowerGun.gd
@@ -8,6 +8,12 @@ onready var sound_shoot = $Shoot
 onready var bullet_spawn_point = $BulletSpawnPoint
 onready var animation_player = $AnimationPlayer
 
+onready var timescale_restore_timer = $TimescaleRestoreTimer
+
+
+func _ready():
+	timescale_restore_timer.connect("timeout", self, "restore_timescale")
+
 
 # ! THIS METHOD IS ONLY CALLED BY PLAYER.GD.
 func add_new_bullet():
@@ -23,9 +29,19 @@ func add_new_bullet():
 	bullet.linear_velocity = direction_to_fire * bullet_velocity
 
 
+func restore_timescale():
+	print("hit!")
+	Engine.time_scale = 1
+
+
 func shoot():
 	if not timer.is_stopped():
 		return false
+
+	timescale_restore_timer.stop()
+	timescale_restore_timer.start()
+
+	Engine.time_scale = 0.2
 
 	add_new_bullet()
 
